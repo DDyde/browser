18aa11fa219b904d4d6be3c948ffae00692ef3f6
DDyde <daniladonut@gmail.com>
2022-03-04 02:27:26 +0300

    changeMenu

--- browser/SettingPage.xaml.cs
+++ browser/SettingPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using browser.MenuPages;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +26,42 @@ namespace browser
         public SettingPage()
         {
             this.InitializeComponent();
+        }
+
+        private void navView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            var selectedItem = (NavigationViewItem)args.SelectedItem;
+            string tag = ((string)selectedItem.Tag);
+
+            if (!args.IsSettingsSelected)
+            {
+                if (tag == "accountItem")
+                {
+                    contentFrame.Navigate(typeof(Account), null, args.RecommendedNavigationTransitionInfo);
+                } else if (tag == "favoriteItem")
+                {
+                    contentFrame.Navigate(typeof(Favorite), null, args.RecommendedNavigationTransitionInfo);
+                }
+                else if (tag == "historyItem")
+                {
+                    contentFrame.Navigate(typeof(History), null, args.RecommendedNavigationTransitionInfo);
+                }
+                else if (tag == "settingItem")
+                {
+                    contentFrame.Navigate(typeof(Setting), null, args.RecommendedNavigationTransitionInfo);
+                }
+                
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void settingPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            navView.SelectedItem = navView.MenuItems[0];
         }
     }
 }
